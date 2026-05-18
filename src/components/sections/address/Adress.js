0bbae39ca7dc8f7b9b76df1d81ee6d9b42483573
payalsatani93 <payalsatani99@gmail.com>
2026-05-18 19:12:00 +0530

    Add contact phone list and update address section with phone numbers

--- src/components/sections/address/Adress.js
+++ src/components/sections/address/Adress.js
@@ -3,6 +3,8 @@ import Link from "next/link";
 import {
   locationUrl,
   manufacturingAddressTitle,
+  officeAddress3,
+  officeAddressTitle,
   officeAddress2,
 } from "@/libs/contactInfo";
 
@@ -11,6 +13,37 @@ const Adress = () => {
     <div className="ltn__contact-address-area mb-90">
       <div className="container">
         <div className="row ltn__contact-address-row">
+          
+          <div className="col-lg-4">
+            <div className="ltn__contact-address-item ltn__contact-address-item-3 box-shadow">
+              <div className="ltn__contact-address-icon">
+                <Image
+                  src="/img/icons/Phone_Icon.jpg"
+                  width={84}
+                  height={82}
+                  alt="Icon Image"
+                />
+              </div>
+              <h3>Phone Number</h3>
+              <p className="contact-phone-list">
+                <span>
+                  <strong>Marketing Manager</strong>
+                  <br />
+                  <Link href="tel:+919904727348">+91 99047 27348</Link>
+                </span>
+                <span>
+                  <strong>Marketing Manager &amp; Management Manager</strong>
+                  <br />
+                  <Link href="tel:+919726687849">+91 97266 87849</Link>
+                </span>
+                <span>
+                  <strong>Purchase &amp; Production Manager</strong>
+                  <br />
+                  <Link href="tel:+917779069188">+91 77790 69188</Link>
+                </span>
+              </p>
+            </div>
+          </div>
           <div className="col-lg-4">
             <div className="ltn__contact-address-item ltn__contact-address-item-3 box-shadow">
               <div className="ltn__contact-address-icon">
@@ -31,22 +64,6 @@ const Adress = () => {
             <div className="ltn__contact-address-item ltn__contact-address-item-3 box-shadow">
               <div className="ltn__contact-address-icon">
                 <Image
-                  src="/img/icons/Phone_Icon.jpg"
-                  width={84}
-                  height={82}
-                  alt="Icon Image"
-                />
-              </div>
-              <h3>Phone Number</h3>
-              <p>
-                +91 99047 27348 <br /> +91 97266 87849 <br />+91 77790 69188
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="ltn__contact-address-item ltn__contact-address-item-3 box-shadow">
-              <div className="ltn__contact-address-icon">
-                <Image
                   src="/img/icons/Address_Icon.jpg"
                   width={84}
                   height={82}
@@ -60,6 +77,13 @@ const Adress = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
+                  <strong className="contact-address-title">
+                    {officeAddressTitle} :
+                  </strong>
+                  <br />
+                  <span>{officeAddress3}</span>
+                  <br />
+                  <br />
                   <strong className="contact-address-title">
                     {manufacturingAddressTitle} :
                   </strong>
