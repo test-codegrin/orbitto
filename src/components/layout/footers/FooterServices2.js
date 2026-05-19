2748ee69cd34781813fd15f103995bb794da087c
payalsatani93 <payalsatani99@gmail.com>
2026-05-19 11:34:03 +0530

    Refactor footer product links into an array for cleaner code

--- src/components/layout/footers/FooterServices2.js
+++ src/components/layout/footers/FooterServices2.js
@@ -1,6 +1,15 @@
 import Link from "next/link";
 import React from "react";
 
+const productLinks = [
+  { name: "Fruits", href: "/products?category=fruit" },
+  { name: "Vegetables", href: "/products?category=vegetable" },
+  { name: "Herbal Powder", href: "/products?category=herbal_powder" },
+  { name: "Spices", href: "/products?category=spices" },
+  { name: "Fruit Powder", href: "/products?category=fruit_powder" },
+  { name: "Honey", href: "/products?category=honey" },
+];
+
 const FooterServices2 = () => {
   return (
     <div className="col-xl-2 col-md-6 col-sm-6 col-12">
@@ -8,24 +17,11 @@ const FooterServices2 = () => {
         <h4 className="footer-title">Our Products</h4>
         <div className="footer-menu">
           <ul>
-            <li>
-              <Link href="/products">Fruits</Link>
-            </li>
-            <li>
-              <Link href="/products">Vegetables</Link>
-            </li>
-            <li>
-              <Link href="/products">Herbal Powder</Link>
-            </li>
-            <li>
-              <Link href="/products">spices</Link>
-            </li>
-            <li>
-              <Link href="/products">Fruite Powder</Link>
-            </li>
-            <li>
-              <Link href="/products">Honey</Link>
-            </li>
+            {productLinks.map(({ name, href }) => (
+              <li key={href}>
+                <Link href={href}>{name}</Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
