a41fb1c03786919d45f9c90be28b966188c378aa
test-codegrin <testcodegrin@gmail.com>
2026-05-24 03:28:07 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
     remotePatterns: [
       {
         protocol: "https",
@@ -18,6 +18,10 @@ const nextConfig = {
       },
     ],
     minimumCacheTTL: 60 * 60 * 24 * 365,
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
