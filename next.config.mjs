fa34c323476a646af58f23b1115cec77e21a6488
test-codegrin <testcodegrin@gmail.com>
2026-05-24 03:30:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    formats: ["image/webp"],
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -17,11 +17,6 @@ const nextConfig = {
         hostname: "*.supabase.co",
       },
     ],
-    minimumCacheTTL: 60 * 60 * 24 * 365,
-    deviceSizes: [640, 750, 828, 1080, 1200],
-    imageSizes: [16, 32, 48, 64, 96],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
